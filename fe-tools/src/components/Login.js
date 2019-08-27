4a5c0be1bdc073b265d77daab1a4265fe44a68fa
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-27 08:43:21 -0600

    I added a logo and header to Login.js

--- fe-tools/src/components/Login.js
+++ fe-tools/src/components/Login.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Form, Image, Grid, Segment, Message } from 'semantic-ui-react';
 import axios from 'axios'
+import logo from '../assets/handyman-tools.png'
+
 
 const Login = (props) => {
     // loginData will store the email and Password input by user and is sent to the backend to verify
@@ -24,22 +26,27 @@ const Login = (props) => {
 
 
     return (
-        <Grid textAlign='center' verticalAlign='middle' style={{height: "100vh"}}>
-            <Grid.Column style={{maxWidth: 500}}>
-                <Form onSubmit={loginSubmit}>
-                    <Segment>
-                        <Form.Input fluid type='email' placeholder='Email' icon='user' iconPosition='left' name='email' value={loginData.email} onChange={changeHandler} required />
-                        <Form.Input fluid type='password' placeholder='Password' icon='lock' iconPosition='left' name='password' value={loginData.password} onChange={changeHandler} required />
+        <div>
+            <Grid textAlign='center' verticalAlign='middle' style={{height: "100vh"}}>
+                <Grid.Column style={{maxWidth: 500}}>
+                    <Image centered src={logo} alt='Logo' style={{width: '100px'}} />
+                    <h1>Use My Tools</h1>
+                    <Form onSubmit={loginSubmit}>
+                        <Segment>
+                            <Form.Input fluid type='text' placeholder='Email' icon='user' iconPosition='left' name='email' value={loginData.email} onChange={changeHandler} required />
+                            <Form.Input fluid type='password' placeholder='Password' icon='lock' iconPosition='left' name='password' value={loginData.password} onChange={changeHandler} required />
 
-                        <Form.Button fluid content='Login' size='large' />
-                    </Segment>
+                            <Form.Button fluid content='Login' size='large' />
+                        </Segment>
 
-                </Form>
-                <Message>
-                    Don't have an account? <Link to='/register'>Sign Up</Link>
-                </Message>
-            </Grid.Column>
-        </Grid>
+                    </Form>
+                    <Message>
+                        Don't have an account? <Link to='/register'>Sign Up</Link>
+                    </Message>
+                </Grid.Column>
+            </Grid>
+            <p><div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"         title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"         title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div></p>
+        </div>
     )
 }
 
