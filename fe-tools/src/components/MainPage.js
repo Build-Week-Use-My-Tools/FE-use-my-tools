fbabf3569786f8465431401b2931720e8e2e38d4
Adan <51211285+Adan011@users.noreply.github.com>
2019-08-28 15:32:23 -0500

    save

--- fe-tools/src/components/MainPage.js
+++ fe-tools/src/components/MainPage.js
@@ -7,14 +7,13 @@ const MainPage = (props) => {
     return (
         <section>
             <h1>MainPage</h1>
-            <h2 onClick={() => {props.history.push('/tooldetail')}}>info</h2>
             <Navigation props={props}/>
             <div id="cards">
                 {props.allToolList.map((tool) => {
-                    return <ToolCard     key={tool.ownerid}
+                    return<ToolCard     key={tool.ownerid}
                                         MainPageprops={tool}
-                    />
-                })}
+                                        />
+                                    })}
             </div>
         </section>
     );
